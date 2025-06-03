4ced2bb412c710e495b669ce089ddb1a75e34eb5
omidsdgi <omid69sdgi@gmail.com>
2025-06-03 20:51:14 +0330

    add props for icon box, size, name, title,badge,link,hideOnMobile,className

--- main-shop/src/components/layouts/header/Header.tsx
+++ main-shop/src/components/layouts/header/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import {Logo, SearchForm} from "@/components";
+import {IconBox} from "@/components/common/ui/icon-box";
 
 export function Header() {
     return (
@@ -11,16 +12,12 @@ export function Header() {
 
                 <ul className="hidden lg:flex gap-5">
                     <li className="flex gap-2 cursor-pointer">
-                        <Link href="#"><i className="icon-user text-[24px]"></i></Link>
-                        <div className="hidden xl:inline-block text-medium text-gray-500 font-lato">Account</div>
+                       <IconBox icon={"icon-user"} size={24} title={"Account"} link={"#"} hideTitleOnMobile={true} />
+
                     </li>
                     <li className="flex gap-2 cursor-pointer">
-                        <div className="relative">
-                            <span
-                                className="absolute -top-[10px] -right-[10px] w-[20px] h-[20px] bg-green-200 rounded-full flex justify-center items-center text-white text-xsmall">4</span>
-                            <Link href="#"><i className="icon-shopping-cart text-[24px]"></i></Link>
-                        </div>
-                        <div className="hidden xl:inline-block text-medium text-gray-500 font-lato">Card</div>
+                        <IconBox icon={'icon-shopping-cart'} link={"#"} hideTitleOnMobile={true} title={'Card'} size={24} badge={0} />
+
                     </li>
                 </ul>
                 <button id="menu_btn" className="flex flex-col justify-between py-[4px] lg:hidden w-[24px] h-[24px]">
