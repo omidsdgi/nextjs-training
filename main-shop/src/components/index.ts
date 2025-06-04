db90a1da167e0e482c873dd13642f8bf72b0ced1
omidsdgi <omid69sdgi@gmail.com>
2025-06-04 21:33:29 +0330

    "feat: implement FeaturedCategories section"

--- main-shop/src/components/index.ts
+++ main-shop/src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './layouts'
 export * from './common'
+export * from './pages'
+
--- main-shop/src/pages/index.tsx
+++ main-shop/src/pages/index.tsx
@@ -1,10 +1,19 @@
-import {Banner, Section} from "@/components";
+import {Banner, FeaturedCategories, Section} from "@/components";
 
 
 export default function Home() {
-  return(
-      <Section className={"container mb-[78px]"}>
-<Banner/>
-      </Section>
-  )
+    return(
+        <>
+            <Section className={"container mb-[78px]"}>
+                <Banner/>
+            </Section>
+
+            <Section className={"container mb-[78px]"}>
+                <div className="hidden sm:flex mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
+                </div>
+                <FeaturedCategories/>
+            </Section>
+        </>
+    )
 }
